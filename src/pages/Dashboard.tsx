7233aa487121174836c82325a3c90498ed166e88
porfiriovitin <victorhugo.svm@hotmail.com>
2026-04-08 21:22:39 -0300

    feat: implement refund fetching and error handling in Dashboard; update state management for refunds and pagination

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+import {api} from "../services/api"
+import {AxiosError} from "axios"
 
 import searchSvg from "../assets/search.svg"
 import { CATEGORIES } from "../utils/categories"
@@ -17,16 +20,42 @@ const REFUND_EXAMPLE = {
     categoryImg: CATEGORIES["food"].icon
 }
 
+const PER_PAGE = 5
+
 export function Dashboard() {
     const [name, setName] = useState("")
     const [page, setPage] = useState(1)
-    const [totalPages, setTotalPages] = useState(10)
-    const [refunds, setRefunds] = useState<RefundItemProps[]> ([REFUND_EXAMPLE])
+    const [totalPages, setTotalPages] = useState(0)
+    const [refunds, setRefunds] = useState<RefundItemProps[]>([])
 
-    function fetchRefunds(e: React.SubmitEvent) {
-        e.preventDefault()
+    async function fetchRefunds() {
+        try {
+            const response = await api.get(`/refunds?name=${name.trim()}&page=${page}&per_page=${PER_PAGE}`)
 
-        console.log(name)
+            const formattedRefunds = response.data.refunds.map((refund: any) => {
+                const key = refund.category as keyof typeof CATEGORIES
+                const categoryData = CATEGORIES[key] ?? CATEGORIES.others
+
+                return {
+                ...refund,
+                category: categoryData.name,
+                categoryImg: categoryData.icon,
+                }
+            })
+
+            setRefunds(formattedRefunds)
+            setTotalPages(response.data.pagination.totalPages)
+
+            console.log(response.data)
+        } catch (error) {
+            console.error("Error fetching refunds:", error)
+
+            if(error instanceof AxiosError && error.response) {
+                alert(`Error: ${error.response?.data.message}`)
+            }
+
+            alert("Ocorreu um erro ao buscar as solicitações. Por favor, tente novamente mais tarde.")
+        }
     }
 
     function handlePagination(action: "next" | "previous") {
@@ -44,6 +73,10 @@ export function Dashboard() {
         })
     }
 
+    useEffect(() => {
+        fetchRefunds()
+    }, [page])
+
     return (
         <div className="bg-gray-500 rounded-xl p-10 md:min-w-3xl w-5xl mx-auto">
             <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações:</h1>
@@ -56,7 +89,7 @@ export function Dashboard() {
 
             <div className="my-5 flex flex-col gap-4 max-h-85.5 overflow-y-scroll p-3">
                 {
-                    refunds.map((item) => <RefundItem key={item.id} data={REFUND_EXAMPLE} href={`/refund/${item.id}`}/> )
+                    refunds.map((item) => <RefundItem key={item.id} data={item} href={`/refund/${item.id}`}/> )
                 }
                 
             </div>
